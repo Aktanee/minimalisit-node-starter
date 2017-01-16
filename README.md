77bf8cafa428d8b325d8fc8eab9253aca58b3610
Antoine Renault <antoine.renault@orson.io>
2017-01-16 12:23:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Minimalist node template using backpack
+
+## How to use
+Install it and run:
+```bash
+npm install
+npm run dev
+```
+
+## How to test
---
+++ src/tests/test_test.js
@@ -0,0 +1,9 @@
+/* eslint-env mocha */
+
+const expect = require('chai').expect
+
+describe('Try mocha', () => {
+  it('should create the cache directory when instantiated', () => {
+    expect(true).to.equal(true)
+  })
+})
